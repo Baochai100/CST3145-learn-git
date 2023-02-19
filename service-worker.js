32f170950bee18c093bdd125a3a208237276cf4b
Bao Chai <chaibao2018@gmail.com>
2023-02-19 16:10:37 +0000

    update

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+var cacheName = "petStore-v1";
+var cacheFiles = [
+  "index.html",
+  "products.js",
+  "chinese.png",
+  "english.png",
+  "icon-192.png",
+  "icon-512.png",
+  "math.png",
+  "music.png",
+  "piano.png",
+];
+
+self.addEventListener("install", function (e) {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      console.log("[Service Worker] Caching files");
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    // check if the cache has the file
+    caches.match(e.request).then(function (r) {
+      if (r) {
+        console.log(
+          "[Service Worker] Resource fetched from the cache for: " +
+            e.request.url
+        );
+        return r;
+      } else {
+        return fetch(e.request).then(function (response) {
+          return caches.open(cacheName).then(function (cache) {
+            cache.put(e.request, response.clone());
+            console.log(
+              "[Service Worker] Resource fetched and saved in the cache for: " +
+                e.request.url
+            );
+            return response;
+          });
+        });
+      }
+    })
+  );
+});
