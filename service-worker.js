40a4e3665c452c1eb94318701db680bb0580c4ce
Bao Chai <chaibao2018@gmail.com>
2023-02-26 16:28:36 +0000

    1

--- service-worker.js
+++ service-worker.js
@@ -4,28 +4,18 @@ var cacheFiles = [
   //"products.js",
   "chinese.png",
   "english.png",
-  "icon-192.png",
+  "favicon.png",
   "icon-512.png",
   "math.png",
   "music.png",
   "piano.png",
 ];
 
-// self.addEventListener("install", function (e) {
-//   console.log("[Service Worker] Install");
-//   e.waitUntil(
-//     caches.open(cacheName).then(function (cache) {
-//       console.log("[Service Worker] Caching files");
-//       return cache.addAll(cacheFiles);
-//     })
-//   );
-// });
-
 self.addEventListener("install", function (e) {
   console.log("[Service Worker] Install");
   e.waitUntil(
     caches.open(cacheName).then(function (cache) {
-      console.log("[Service Worker] Caching all the files");
+      console.log("[Service Worker] Caching files");
       return cache.addAll(cacheFiles);
     })
   );
@@ -56,31 +46,3 @@ self.addEventListener("install", function (e) {
 //     })
 //   );
 // });
-self.addEventListener("fetch", function (e) {
-  e.respondwith(
-    caches.match(e.request).then(function (cachedFile) {
-      //download the file if it is not in the cache
-      if (cachedFile) {
-        console.log(
-          " [Service Worker] Resource fetched from the cache for: " +
-            e.request.url
-        );
-
-        return cachedFile;
-      } else {
-        return fetch(e.request).then(function (response) {
-          return caches.open(cacheName).then(function (cache) {
-            //add the new file to the cache
-            cache.put(e.request, response.clone());
-            console.log(
-              "[Service Worker] Resource fetched and saved in the cache for:" +
-                e.request.url
-            );
-
-            return response;
-          });
-        });
-      }
-    })
-  );
-});
