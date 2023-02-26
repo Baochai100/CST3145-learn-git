286f7e15fa877acab62f23db7e90def56e995646
Bao Chai <chaibao2018@gmail.com>
2023-02-26 15:51:15 +0000

    1

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 var cacheName = "afterschool-v1";
 var cacheFiles = [
   "index.html",
-  "products.js",
+  //"products.js",
   "chinese.png",
   "english.png",
   "icon-192.png",
@@ -12,34 +12,72 @@ var cacheFiles = [
   "afterschool.webmanifest",
 ];
 
+// self.addEventListener("install", function (e) {
+//   console.log("[Service Worker] Install");
+//   e.waitUntil(
+//     caches.open(cacheName).then(function (cache) {
+//       console.log("[Service Worker] Caching files");
+//       return cache.addAll(cacheFiles);
+//     })
+//   );
+// });
+
 self.addEventListener("install", function (e) {
   console.log("[Service Worker] Install");
   e.waitUntil(
     caches.open(cacheName).then(function (cache) {
-      console.log("[Service Worker] Caching files");
+      console.log("[Service Worker] Caching all the files");
       return cache.addAll(cacheFiles);
     })
   );
 });
 
+// self.addEventListener("fetch", function (e) {
+//   e.respondWith(
+//     // check if the cache has the file
+//     caches.match(e.request).then(function (cachedFile) {
+//       if (cachedFile) {
+//         console.log(
+//           "[Service Worker] Resource fetched from the cache for: " +
+//             e.request.url
+//         );
+//         return cachedFile;
+//       } else {
+//         return fetch(e.request).then(function (response) {
+//           return caches.open(cacheName).then(function (cache) {
+//             cache.put(e.request, response.clone());
+//             console.log(
+//               "[Service Worker] Resource fetched and saved in the cache for: " +
+//                 e.request.url
+//             );
+//             return response;
+//           });
+//         });
+//       }
+//     })
+//   );
+// });
 self.addEventListener("fetch", function (e) {
-  e.respondWith(
-    // check if the cache has the file
+  e.respondwith(
     caches.match(e.request).then(function (cachedFile) {
+      //download the file if it is not in the cache
       if (cachedFile) {
         console.log(
-          "[Service Worker] Resource fetched from the cache for: " +
+          " [Service Worker] Resource fetched from the cache for: " +
             e.request.url
         );
+
         return cachedFile;
       } else {
         return fetch(e.request).then(function (response) {
           return caches.open(cacheName).then(function (cache) {
+            //add the new file to the cache
             cache.put(e.request, response.clone());
             console.log(
-              "[Service Worker] Resource fetched and saved in the cache for: " +
+              "[Service Worker] Resource fetched and saved in the cache for:" +
                 e.request.url
             );
+
             return response;
           });
         });
