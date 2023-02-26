073a813886a2d26847134c7981b06f7e139773c9
Bao Chai <chaibao2018@gmail.com>
2023-02-26 17:17:01 +0000

    update

--- service-worker.js
+++ service-worker.js
@@ -21,28 +21,28 @@ self.addEventListener("install", function (e) {
   );
 });
 
-// self.addEventListener("fetch", function (e) {
-//   e.respondWith(
-//     // check if the cache has the file
-//     caches.match(e.request).then(function (cachedFile) {
-//       if (cachedFile) {
-//         console.log(
-//           "[Service Worker] Resource fetched from the cache for: " +
-//             e.request.url
-//         );
-//         return cachedFile;
-//       } else {
-//         return fetch(e.request).then(function (response) {
-//           return caches.open(cacheName).then(function (cache) {
-//             cache.put(e.request, response.clone());
-//             console.log(
-//               "[Service Worker] Resource fetched and saved in the cache for: " +
-//                 e.request.url
-//             );
-//             return response;
-//           });
-//         });
-//       }
-//     })
-//   );
-// });
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    // check if the cache has the file
+    caches.match(e.request).then(function (cachedFile) {
+      if (cachedFile) {
+        console.log(
+          "[Service Worker] Resource fetched from the cache for: " +
+            e.request.url
+        );
+        return cachedFile;
+      } else {
+        return fetch(e.request).then(function (response) {
+          return caches.open(cacheName).then(function (cache) {
+            cache.put(e.request, response.clone());
+            console.log(
+              "[Service Worker] Resource fetched and saved in the cache for: " +
+                e.request.url
+            );
+            return response;
+          });
+        });
+      }
+    })
+  );
+});
